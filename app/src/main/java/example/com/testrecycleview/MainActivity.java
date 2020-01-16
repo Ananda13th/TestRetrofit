cafcdba684daa02b87f80af64feee097ed1afe5b
Ananda <pramonoananda@gmail.com>
2020-01-16 13:36:34 +0700

    Detail+Update Dosen

--- app/src/main/java/example/com/testrecycleview/MainActivity.java
+++ app/src/main/java/example/com/testrecycleview/MainActivity.java
@@ -1,5 +1,7 @@
 package example.com.testrecycleview;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -20,6 +22,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -29,6 +32,7 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
@@ -38,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private MyAdapter adapter;
+    ActionBar toolbar;
     //pakai arraylist agar tidak error waktu null
     private List<Dosen> dosenArrayList;
     private ClickListener listener;
@@ -46,6 +51,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigationView);
+        bottomNavigationView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.navigation_home:
+                        Toast.makeText(MainActivity.this, "Album", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.navigation_artists:
+                        Toast.makeText(MainActivity.this, "Favorites", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.navigation_works:
+                        Toast.makeText(MainActivity.this, "Nearby", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+            });
         final Button delete_button = findViewById(R.id.button_delete);
         final ApiInterface service = ApiClient.getClient().create(ApiInterface.class);
         Call<ResponseDosen> call = service.getDataDosen();
